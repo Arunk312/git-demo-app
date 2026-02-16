0320abca9d2eae4e5d36d85f2329243b50d5c42a
Arun Kumary Maurya <akmaurya312312@gmail.com>
2026-02-16 09:21:58 +0530

    cange the image

--- app.py
+++ app.py
@@ -8,10 +8,10 @@ with col1:
     st.image('cuty.jpg',width=300)
 
 with col2:
-    st.write('One of my noughty student')
+    st.write("I love her more the any thing in the world and i can't giveup my dream to find her")
 
 with col3:
-    st.write(' she is bad at studey , i can imagin where is study  will take her')
+    st.write(' she is my crush and i love her from childhood i pray to god take all what i have and give her to my ')
 
 
 st.header('courses')
