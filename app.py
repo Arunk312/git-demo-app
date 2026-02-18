6a1b403abedefdd8379034fe27e37e9cd26de3e4
Arun Kumary Maurya <akmaurya312312@gmail.com>
2026-02-18 09:58:49 +0530

    add sidebar and sidebar menu

--- app.py
+++ app.py
@@ -8,13 +8,18 @@ with col1:
     st.image('cuty.jpg',width=300)
 
 with col2:
-    st.write("I love her more the any thing in the world and i can't giveup my dream to find her")
+    st.write('One of my noughty student')
 
 with col3:
-    st.write(' she is my crush and i love her from childhood i pray to god take all what i have and give her to my ')
+    st.write(' she is bad at studey , i can imagin where is study  will take her')
 
 
 st.header('courses')
 st.header('Data science')
-st.header('apply machine learing')
-st.header('apply tag for version')
+st.sidebar.title('menu')
+st.sidebar.markdown("""
+- Home
+- about 
+- contect
+""")
+st.sidebar.write('sidebar create ot test delete branch')
